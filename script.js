ed507149855c44edf91d3a6ce2e79a0f1608fbb6
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-05-13 15:56:01 +0100

    Menu stays open when on items

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Function to open dropdown menu
 
-function dropdown(input, show, display) {
-  const dropdownItems = document.querySelector(`${show}`);
-  document.querySelector(`${input}`).addEventListener("mouseover", () => {
-    dropdownItems.style.display = `${display}`;
+function dropdown(input, show, displayType) {
+  const dropdownItems = document.querySelector(show);
+  const inputItem = document.querySelector(input);
+  inputItem.addEventListener("mouseover", () => {
+    dropdownItems.style.display = displayType;
+  });
+  inputItem.addEventListener("mouseout", () => {
+    dropdownItems.style.display = "none";
   });
 }
+
+dropdown("#hover-area", "#dropdown-list", "flex");
